febd4ae94aec616db27870d45df2cbb72c12e0f9
Sirqasim <m.qasim077@gmail.com>
2017-08-03 20:44:53 +0500

    MITI Class2 M15

---
+++ JavaScript/class2/js/abc.js
@@ -0,0 +1,5 @@
+// JavaScript Document
+var mobileMenu='<ul><li><a href="#">Color</a></li><li><a href="#">Model</a></li><li><a href="#">Price</a></li><li><a href="#">Rating</a></li></ul>';
+
+var carMenu='<ul><li><a href="#">Car body Color</a></li><li><a href="#">Model and software version</a></li><li><a href="#">Price in $ </a></li><li><a href="#">Rating</a></li></ul>';
+
