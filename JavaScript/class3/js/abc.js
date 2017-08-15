f972868fb8c06c136b4900562f14c29bc1c84c9c
Sirqasim <m.qasim077@gmail.com>
2017-08-15 20:33:41 +0500

    MARKS SHEET WITH IF CONDITION

--- JavaScript/class3/js/abc.js
+++ JavaScript/class3/js/abc.js
@@ -10,9 +10,22 @@ function abc(){
 	var grade="";
 	
 	
+	if(per >=0 && per < 40){
+	grade="Fail";	
+} else if( per >= 40  && per < 50){
+	grade="D";
+}else if(per >=50 && per < 60){
+	grade="C";
+}else if(per >=60  && per < 70){
+	grade="B";
+}else if(per >=70 && per < 80){
+	grade="A";
+}else if(per >=80 && per <= 100){
+	grade="A+";
+}
+
 	
 	
 	
-	
-	document.getElementById("result").innerHTML="Total Marks "+totalMarks+" Obtained Marks is "+obtainedMarks+" your percentage is "+per+"%";
+	document.getElementById("result").innerHTML="Total Marks "+totalMarks+" Obtained Marks is "+obtainedMarks+" your percentage is "+per+"% YOUR GRADE IS <span>"+grade+"</span>";
 }
